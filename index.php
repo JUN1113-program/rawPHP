b50f3b982baa12b2ddac67dfda4e99eed719968c
JUN1113 <dunkelrots@gmail.com>
2019-12-02 21:44:26 +0900

    クラスを定義し、インスタンスを生成

--- index.php
+++ index.php
@@ -7,3 +7,26 @@
   echo 1+1;
 ?>
 <br>
+<?php
+// class Teaを宣言、お茶の名前を持つ
+  class Tea{
+    public $name;
+    public function __construct($name){
+      $this->name = $name;
+    }
+  }
+
+// teaNamesを定義する
+  $TeaNames = array("綾鷹","生茶","紅茶");
+
+// 定義したTeaNamesの各要素に対してインスタンスを生成し、名前を出力
+  foreach($TeaNames as $tea){
+    $tea = new Tea($tea);
+    echo $tea->name;
+  }
+
+// 配列をそのまま出力する
+  foreach($TeaNames as $tea){
+    echo $tea;
+  }
+?>
